dcaaf8176a3054161339b46eda2245130961998f
zephkelly <masterevank@gmail.com>
2022-12-31 23:18:59 +1100

    Populate file structure, setup server.js

---
+++ server/server.ts
@@ -0,0 +1,25 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+const PORT = 62264;
+
+app.use(cookieParser());
+app.use(express.static('./ochre-ivy/dist'));
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html');
+});
+//Routes
+app.get('/setuser', (req, res) => {
+  res.cookie('user', 'John');
+  res.send('User is set');
+});
+
+app.get('/getuser', (req, res) => {
+  res.send(req.cookies);
+});
+
+app.listen(PORT, () => {
+  console.log('Example app listening on port ' + PORT);
+});
