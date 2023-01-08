3d4070dced2fffc644c69efbcb9c4fe632185ed0
zephkelly <masterevank@gmail.com>
2023-01-08 23:47:17 +1100

    Setup mongodb connection with blog routing and user auth

---
+++ src/server/auth.ts
@@ -0,0 +1,5 @@
+export function isAuthenticated(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next();
+  }
+}
--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,28 @@
+import { isAuthenticated } from "./auth";
+
 const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const PORT = 62264;
 
-//Static files
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('./'));
+
+//Database
+mongoose.connect('mongodb://localhost:27017/ochre-ivy', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const blogSchema = {
+  title: String,
+  content: String,
+};
+
+const Blog = mongoose.model('Blog', blogSchema);
 
 //Middleware
 
@@ -14,7 +32,43 @@ app.get('/', (req, res) => {
 });
 
 app.get('/blog', (req, res) => {
-  res.sendFile('blog.html');
+  Blog.find({}, (err, blogs) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.send('Hey there! Here are all the blog posts: ');
+      res.json(blogs);
+    }
+  });
+});
+
+app.get('/blog/:blogTitle', (req, res) => {
+  Blog.find({ title: req.params.blogTitle },
+    (err, blog) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(blog);
+      }
+    })
+})
+
+app.post('/blog', isAuthenticated, (req, res) => {
+  const title = req.body.title;
+  const content = req.body.content;
+
+  const blog = new Blog({
+    title: title,
+    content: content,
+  });
+
+  blog.save((err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('Successfully added a new blog post.');
+    }
+  });
 });
 
 app.listen(PORT, () => {
