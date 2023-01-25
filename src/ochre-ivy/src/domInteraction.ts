6d5901f2d5558476e9081578be3ecb5dec76d75a
zephkelly <masterevank@gmail.com>
2023-01-25 11:08:20 +1100

    Fix scroll-linked positioning effect

--- src/ochre-ivy/src/domInteraction.ts
+++ src/ochre-ivy/src/domInteraction.ts
@@ -9,7 +9,6 @@ const navMenuBtn: HTMLElement= document.querySelector(".nav-menu-btn") as HTMLEl
 // -- NAVIGATION BAR ----
 // -- Shrinking navbar on scroll
 let shrunkNavbar: boolean = false;
-let navBarShrinkTimeout: boolean = false;
 
 (function setNavbarInitial() {
   window.addEventListener('load', () => {
@@ -17,53 +16,50 @@ let navBarShrinkTimeout: boolean = false;
   });
 })();
 
+let scrollY = 0;
 export function shrinkNavbar() {
-  if (navBarShrinkTimeout) return;
-
-  (function callTimeout() {
-    navBarShrinkTimeout = true;
-    delay(300).then(() => navBarShrinkTimeout = false);
-  });
-
-  let scrollY: number = document.documentElement.scrollTop;
-
-  if (scrollY > 30) {
-    if (shrunkNavbar) return;
+  delay(100)
+    .then(() => scrollY = document.documentElement.scrollTop)
+    .then(() => {
+      if (scrollY > 20) {
+        if (shrunkNavbar) return;
         
-    manipulateNavDOM(
-      '0rem 0rem 2rem 0rem rgba(0, 0, 0, 0.05)',
-      '3rem', '1.4rem', '1.8rem', true
-    );
-  } else {
-    if (!shrunkNavbar) return;
-    manipulateNavDOM('', '', '', '', false);
-  }
+        manipulateNavDOM(
+          '0rem 0rem 2rem 0rem rgba(0, 0, 0, 0.05)',
+          '3rem', '1.4rem', '1.8rem', true
+        );
+       } else {
+        if (!shrunkNavbar) return;
+        manipulateNavDOM('', '', '', '', false);
+      }
 
-  function manipulateNavDOM(bShadow: string, height: string, tFont: string, bFont: string, shrunkState: boolean) {
-    navbar.style.boxShadow = bShadow;
-    navbar.style.height = height;
-    navText.style.fontSize = tFont;
-    navMenuBtn.style.fontSize = bFont;
-    shrunkNavbar = shrunkState;
+      function manipulateNavDOM(bShadow: string, height: string, tFont: string, bFont: string, shrunkState: boolean) {
+        navbar.style.boxShadow = bShadow;
+        navbar.style.height = height;
+        navText.style.fontSize = tFont;
+        navMenuBtn.style.fontSize = bFont;
+        shrunkNavbar = shrunkState;
 
-    //So we dont get a weird transition on load
-    navbar.style.transition = "all 0.3s ease-out";
+        //So we dont get a weird transition on load
+        navbar.style.transition = "all 0.3s ease-out";
 
-    if (shrunkNavbar) {
-      navbar.style.backgroundColor = '';
-      navText.style.marginBottom = '0';
-      navTextSpacer.style.opacity = '0';
-      navTextSpacer.style.bottom = '-0.5rem';
-      navTextSpacer.style.left = '0.5rem';
-      navTextSpacer.style.height = '0';
+        if (shrunkNavbar) {
+          navbar.style.backgroundColor = '';
+          navText.style.marginBottom = '0';
+          navTextSpacer.style.opacity = '0';
+          navTextSpacer.style.bottom = '-0.5rem';
+          navTextSpacer.style.left = '0.5rem';
+          navTextSpacer.style.height = '0';
 
-    } else {
-      navbar.style.backgroundColor = '#f8c6b7';
-      navText.style.marginBottom = '';
-      navTextSpacer.style.opacity = '';
-      navTextSpacer.style.bottom = '';
-      navTextSpacer.style.left = '';
-      navTextSpacer.style.height = '';
-    }
-  }
+        } else {
+          navbar.style.backgroundColor = '#f8c6b7';
+          navText.style.marginBottom = '';
+          navTextSpacer.style.opacity = '';
+          navTextSpacer.style.bottom = '';
+          navTextSpacer.style.left = '';
+          navTextSpacer.style.height = '';
+        }
+      }
+  });  
 }
+
