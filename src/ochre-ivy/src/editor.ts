89459d33c9119b27522632e03d441f5ea29622f3
zephkelly <masterevank@gmail.com>
2023-02-01 19:06:45 +1100

    Import additional plugins for editorjs
    Header, Quote, Image, List

--- src/ochre-ivy/src/editor.ts
+++ src/ochre-ivy/src/editor.ts
@@ -1,7 +1,18 @@
-import EditorJS from '../node_modules/@editorjs/editorjs';
+import EditorJS from '@editorjs/editorjs';
+import Header from '@editorjs/header';
+import Image from '@editorjs/image';
+import Quote from '@editorjs/quote';
+import List from '@editorjs/list';
 
 const editor = new EditorJS({
-  minHeight : 0
+  holder: 'editorjs',
+  minHeight: 120,
+  tools: {
+    header: Header,
+    quote: Quote,
+    list: List,
+    image: Image,
+  },
 });
 
 if (editor) {
---
+++ src/ochre-ivy/src/types/index.d.ts
@@ -0,0 +1,3 @@
+declare module '@editorjs/image';
+declare module '@editorjs/quote';
+declare module '@editorjs/list';
